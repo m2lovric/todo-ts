fe3592320a9e976175c089078a970e729482c0cd
m2lovric <m2lovric@gmail.com>
2022-01-19 22:59:47 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  interface User {
+    id?: number;
+    name: string;
+    age: number;
+    job: string;
+  }
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [job, setJob] = useState('');
+  const [data, setData] = React.useState<User[]>([]);
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user: User = { name, age, job };
+
+    fetch('http://localhost:8000/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+  };
+
+  useEffect(() => {
+    fetch('http://localhost:8000/user')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={handleSubmit}>
+        <label>Name</label>
+        <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+        <label>Age</label>
+        <input type='number' value={age} onChange={(e) => setAge(parseInt(e.target.value))} />
+        <label>Job</label>
+        <input type='text' value={job} onChange={(e) => setJob(e.target.value)} />
+        <button>Add user</button>
+      </form>
+
+      <main>
+        {data &&
+          data.map((el) => {
+            return (
+              <article key={el.id}>
+                <h2>{el.name}</h2>
+                <p>{el.age}</p>
+                <p>{el.job}</p>
+              </article>
+            );
+          })}
+      </main>
     </div>
   );
 }
